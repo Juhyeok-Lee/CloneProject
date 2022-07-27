1659c826a05799532308729a18d73d4993fcee7f
Juhyeok LEE <songari70@gmail.com>
2022-07-27 14:59:55 +0900

    0727 알고리즘 문제 2번까지 해결 완료. 3번 풀이 중.

---
+++ Problem0727/Problem_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1번 문제. N개의 숫자가 공백 없이 쓰여있다.
+            // 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+            Console.WriteLine("숫자의 개수를 입력하세요.");
+            int a = int.Parse(Console.ReadLine());
+            int[] array = new int[a];
+            int sum = 0;
+
+            Console.WriteLine("숫자를 차례로 적어주세요.");
+            string str = Console.ReadLine();
+            for (int i = 0; i < a; ++i)
+            {
+                array[i] = int.Parse(str.Substring(i, 1));
+            }
+
+            for (int i = 0; i < a; ++i)
+            {
+                sum += array[i];
+            }
+
+            Console.WriteLine("숫자의 합은 " + sum + "입니다.");
+        }
+    }
+}
---
+++ Problem0727/Problem_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2번 문제. 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력.
+            // 윤년은 연도가 4의 배수이면서 100의 배수가 아닐 때,
+            // 또는 400의 배수일 때이다.
+            int year = 0;
+            int leafYear = 0;
+
+            while (true)
+            {
+                Console.WriteLine("연도를 입력하세요.");
+                year = int.Parse(Console.ReadLine());
+                if (year < 1 || year > 4000)
+                {
+                    Console.WriteLine("올바른 연도를 입력하세요.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (year % 4 == 0 && year % 100 != 0) leafYear = 1;
+            else if (year % 400 == 0) leafYear = 1;           
+
+            Console.WriteLine(leafYear);
+        }
+    }
+}
