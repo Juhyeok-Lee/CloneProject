5cbd3a2348ab9e8750a4bd44c91c975158b037c2
Juhyeok_Lee <songari70@gmail.com>
2022-07-27 23:28:57 +0900

    0727 알고리즘 문제 3번 해결 완료.
    - 알고리즘을 변경하여 공간복잡도와 시간복잡도를 대폭 낮춤.
    - char 배열 혹은  string을 반환하는 알고리즘, 혹은 함수 내부에서 직접 문자를 출력하는  알고리즘은 메모리를 매우 많이 사용하였음.
    - 재귀함수에 한 번에 큰 값을 input하면 스택이 매우 커져 연산이 매우 느려짐. 대신 for문 내부에서 재귀함수를 여러 번 출력하게 함으로써 한 번 호출에 걸리는 시간과 메모리를 줄였음.
    - 다른 프랙탈 도형에도 같은 알고리즘을 사용할 수 있을 지는 확실치 않음.

--- Problem0727/Problem_3/Program.cs
+++ Problem0727/Problem_3/Program.cs
@@ -3,80 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Problem_3
 {
-    class Program
+    internal class Program
     {
-        static char[,] SquareSierpinski(int size)
+        // System.io를 추가하고 스트림리더와 스트림라이터를 사용하여 공간복잡도를 감소시킴.
+        static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        static void SquareSierpinski(int _i, int _j, int _N)
         {
-            char[,] sqr;
-            if (size == 3)
+            if ((int)(_i / _N) % 3 == 1 && (int)(_j / _N) % 3 == 1)
             {
-                sqr = new char[3, 3];
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        if (i == 1 && j == 1)
-                        {
-                            sqr[i, j] = ' ';
-                        }
-                        else
-                        {
-                            sqr[i, j] = '*';
-                        }
-                    }
-                }
-                return sqr;
+                sw.Write(" ");
             }
             else
             {
-                sqr = new char[size, size];
-                char[,] sqr_1 = SquareSierpinski(size / 3);
-                for (int i = 0; i < size; i++)
+                if (_N / 3 < 1)
+                    sw.Write("*");
+                else
                 {
-                    for (int j = 0; j < size; j++)
-                    {
-                        if (i < size * 2 / 3 && i >= size * 1 / 3)
-                        {
-                            if (j < size * 2 / 3 && j >= size * 1 / 3)
-                            {
-                                sqr[i, j] = ' ';
-                            }
-                            else
-                            {
-                                sqr[i, j] = sqr_1[i % (size / 3), j % (size / 3)];
-                            }
-                        }
-                        else
-                        {
-                            sqr[i, j] = sqr_1[i % (size / 3), j % (size / 3)];
-                        }
-                    }
+                    SquareSierpinski(_i, _j, _N / 3);
                 }
-                return sqr;
             }
-
-
         }
+
 
         static void Main(string[] args)
         {
-            int N;
-            Console.WriteLine("N의 값을 입력하세요. (N은 3의 거듭제곱)");
-            N = int.Parse(Console.ReadLine());
-            char[,] sierSqr = new char[N, N];
+            // 스트림라이터를 사용하니 에러가 발생하여 Console.WriteLine을 사용함.
+            Console.WriteLine("N을 입력하세요. (N은 3의 거듭제곱)");
 
-            for (int i = 0; i < N; i++)
+            int N = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < N; i++)                                     // 한 행씩 작성하는 것을 N만큼 반복
             {
-                for (int j = 0; j < N; j++)
-                {
-                    Console.Write(SquareSierpinski(N)[i, j]);
-                }
-                Console.Write("\n");
+                for (int j = 0; j < N; j++) SquareSierpinski(i, j, N);      // 한 행 작성
+                sw.Write("\n");                                             // 한 줄마다 줄바꿈 문자를 추가해줌.
             }
 
+            // 스트림리더와 스트림라이터는 사용자가 직접 닫아줘야 함.
+            sr.Close();
+            sw.Close();
         }
     }
 }
