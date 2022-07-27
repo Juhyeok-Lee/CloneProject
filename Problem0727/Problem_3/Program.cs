b233cdc1882c060ff446eedc17ad36fa6e06d27d
Juhyeok_Lee <songari70@gmail.com>
2022-07-27 18:05:58 +0900

    0727 알고리즘 문제 전부 해결. 콘솔 창의 크기 한계 탓에 N=81까지 제대로 작동하는 것만 확인하였음.

--- Problem0727/Problem_3/Program.cs
+++ Problem0727/Problem_3/Program.cs
@@ -8,19 +8,75 @@ namespace Problem_3
 {
     class Program
     {
-        string SquareSierpinski(int size)
+        static char[,] SquareSierpinski(int size)
         {
+            char[,] sqr;
             if (size == 3)
             {
-                string str;
-                str=str+ "*"*size;
-                return str;
+                sqr = new char[3, 3];
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i == 1 && j == 1)
+                        {
+                            sqr[i, j] = ' ';
+                        }
+                        else
+                        {
+                            sqr[i, j] = '*';
+                        }
+                    }
+                }
+                return sqr;
             }
-            return SquareSierpinski(size / 3);
+            else
+            {
+                sqr = new char[size, size];
+                char[,] sqr_1 = SquareSierpinski(size / 3);
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (i < size * 2 / 3 && i >= size * 1 / 3)
+                        {
+                            if (j < size * 2 / 3 && j >= size * 1 / 3)
+                            {
+                                sqr[i, j] = ' ';
+                            }
+                            else
+                            {
+                                sqr[i, j] = sqr_1[i % (size / 3), j % (size / 3)];
+                            }
+                        }
+                        else
+                        {
+                            sqr[i, j] = sqr_1[i % (size / 3), j % (size / 3)];
+                        }
+                    }
+                }
+                return sqr;
+            }
+
+
         }
 
         static void Main(string[] args)
         {
+            int N;
+            Console.WriteLine("N의 값을 입력하세요. (N은 3의 거듭제곱)");
+            N = int.Parse(Console.ReadLine());
+            char[,] sierSqr = new char[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(SquareSierpinski(N)[i, j]);
+                }
+                Console.Write("\n");
+            }
+
         }
     }
 }
