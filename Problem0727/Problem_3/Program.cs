47b90351b340460d5ae49d235301bc4af00ef112
Juhyeok LEE <songari70@gmail.com>
2022-07-27 15:21:00 +0900

    0727 알고리즘 시어핀스키 문제 코드 작성 중.

--- Problem0727/Problem_3/Program.cs
+++ Problem0727/Problem_3/Program.cs
@@ -8,6 +8,17 @@ namespace Problem_3
 {
     class Program
     {
+        string SquareSierpinski(int size)
+        {
+            if (size == 3)
+            {
+                string str;
+                str=str+ "*"*size;
+                return str;
+            }
+            return SquareSierpinski(size / 3);
+        }
+
         static void Main(string[] args)
         {
         }
