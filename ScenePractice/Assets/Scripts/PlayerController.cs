417a2821c6c1d50aa955f6bade2f1c5c8eb787dc
Juhyeok_Lee <songari70@gmail.com>
2022-08-02 19:57:59 +0900

    0802 팀프로젝트 모바일 테스트용 씬 생성.
    - 조이스틱을 통한 플레이어 오브젝트 움직임 구현
    - 화면 드래그를 통한 카메라 회전 구현

---
+++ ScenePractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody),typeof(BoxCollider))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+
+    [SerializeField] private float _moveSpeed;
+
+    public Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            transform.forward = cam.forward;
+            _rigidbody.velocity = transform.localRotation * new Vector3(
+                _joystick.Horizontal * Time.deltaTime * 100f, 0,
+                _joystick.Vertical * Time.deltaTime * 100f);
+        }     
+    }
+}
